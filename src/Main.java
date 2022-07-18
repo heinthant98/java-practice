53eeca9a09e370a9c6e55728e93a77a69ca68466
Hein Thant <heinthant098@gmail.com>
2022-07-18 18:26:42 +0630

    pull main

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,29 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 		List<User> users = List.of(
 				new User("佐藤美咲", LocalDate.of(1990, 1, 1)),
-				new User("鈴木太郎", LocalDate.of(1991, 2, 2)),
+				new User("鈴木太郎", LocalDate.of(1991, 2, 2)), 
 				new User("山田一郎", LocalDate.of(2003, 3, 3)),
 				new User("鈴木花子", LocalDate.of(2002, 4, 4)));
-		
+
 		System.out.println("【すべてのユーザーを表示する】");
 		users.forEach(u -> System.out.println("名前: %s, 生年月日: %s".formatted(u.getName(), u.getBirthDate())));
-	
+
 		System.out.println("【すべてのユーザーを表示する。ただし生年月日はyyyy/MM/dd(E)形式で出力する】");
-		users.forEach(u -> System.out.println("名前: %s, 生年月日: %s".formatted(u.getName(), toJapaneseFormat(u.getBirthDate()))));
-		
+		users.forEach(
+				u -> System.out.println("名前: %s, 生年月日: %s".formatted(u.getName(), toJapaneseFormat(u.getBirthDate()))));
+
 		System.out.println("【名前が鈴木で始まる人のみを表示する】");
-		users.stream().filter(user ->user.getName().startsWith("鈴木"))
-						.forEach(u -> System.out.println(u.getName()));
-	}
-	
+		users.stream().filter(user -> user.getName().startsWith("鈴木")).forEach(u -> System.out.println(u.getName()));
+
+}
+
 	private static String toJapaneseFormat(LocalDate localDate) {
 		return localDate.format(DateTimeFormatter.ofPattern("yyyy年MM月dd日(E)"));
 	}
