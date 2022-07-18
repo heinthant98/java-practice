393111ed8147b3b4ba8e30238da446374f603b07
Hein Thant <heinthant098@gmail.com>
2022-07-18 13:27:48 +0630

    Displaying LocalDate in Japanese

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
 
@@ -13,6 +14,13 @@ public class Main {
 		
 		System.out.println("【すべてのユーザーを表示する】");
 		users.forEach(u -> System.out.println("名前: %s, 生年月日: %s".formatted(u.getName(), u.getBirthDate())));
+	
+		System.out.println("【すべてのユーザーを表示する。ただし生年月日はyyyy/MM/dd(E)形式で出力する】");
+		users.forEach(u -> System.out.println("名前: %s, 生年月日: %s".
+							formatted(u.getName(),
+										u.getBirthDate().format(DateTimeFormatter.ofPattern("yyyy年MM月dd日(E)")
+												.localizedBy(Locale.JAPAN)))));
+		
 	}
 
 }
